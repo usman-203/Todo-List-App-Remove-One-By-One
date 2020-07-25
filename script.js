0fb0a0baa09f6adfd29a7b991a7720d5548dcb7d
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-07-25 07:27:27 +0500

    commit3

--- script.js
+++ script.js
@@ -30,3 +30,36 @@ add_todo_btn.addEventListener('click', function () {
     edit_btn.classList.add('action-btn');
     edit_btn.classList.add('edit-btn');
     edit_btn.textContent = 'EDIT';
+
+    //Edit todo;
+    edit_btn.addEventListener('click', function () {
+        edit_input.classList.remove('hide');
+        item_text.classList.add('hide');
+        edit_input_btn.classList.remove('hide');
+        edit_input_btn.addEventListener('click', function () {
+            item_text.textContent = edit_input.value;
+            edit_input.classList.add('hide');
+            item_text.classList.remove('hide');
+            edit_input_btn.classList.remove('hide');
+        });
+    });
+    var remove_btn = document.createElement('BUTTON');
+    remove_btn.classList.add('action-btn');
+    remove_btn.classList.add('remove-btn');
+    remove_btn.textContent = 'Remove';
+
+    //Remove todo;
+
+    remove_btn.addEventListener('click', function () {
+        item.parentNode.removeChild(item);
+    });
+    action_btns.append(edit_input_btn);
+    action_btns.append(edit_btn);
+    action_btns.append(remove_btn);
+    item.append(item_text);
+    item.append(edit_input);
+    item.append(action_btns);
+    list.append(item);
+
+    todo_input.value = ''
+});
